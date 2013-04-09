6f5be98593bad619e099d0ba07c021652b89fa57
Rich Trott <rtrott@gmail.com>
2013-04-08 17:18:19 -0700

    merge orthosurg news changes from current UCSF Mobile

--- root/orthosurg/news/news.hogan.src.js
+++ root/orthosurg/news/news.hogan.src.js
@@ -2,23 +2,23 @@
 var hogan = require("hogan.js");
 
 // construct template string
-var template = '{{#feed}}';
+var template = '{{#title}}';
     template += '<div class="menu detailed">';
     template += '<h2>{{title}}</h2>';
     template += '<ol>';
-    template += '{{#entries}}';
+    template += '{{/title}}';
+    template += '{{#post}}';
     template += '  <li>';
-    template += '    <a href="{{link}}"><span class="external">{{title}}</span>';
-    template += '    <div class="smallprint light">{{contentSnippet}}</div></a>';
-    template += '{{/entries}}';
+    template += '    <a href="{{POST_LINK}}"><span class="external">{{POST_TITLE}}</span>';
+    template += '    <div class="smallprint light">{{POST_DATE}}</div></a>';
+    template += '{{/post}}';
     template += '</ol>';
     template += '</div>';
-    template += '{{/feed}}';
-    template += '{{^feed}}';
+    template += '{{^post}}';
     template += '<div class="content">';
     template += '<p>Content could not be loaded.</p>';
     template += '</div>';
-    template += '{{/feed}}';
+    template += '{{/post}}';
 
 // compile template
 var output = hogan.compile(template, {asString: true});
