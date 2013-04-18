5ecda910bc31116f3ef7e3a08e3eba617117e06d
Rich Trott <rtrott@gmail.com>
2013-04-17 17:02:46 -0700

    uniform naming

--- root/assets/js/ucsf/shuttle.src.js
+++ root/assets/js/ucsf/shuttle.src.js
@@ -20,7 +20,7 @@ ucsf.shuttle = (function () {
     };
 
     me.renderForm = function (response) {
-        var form = document.getElementById('ucsf_shuttle_trip_form');
+        var formContainer = document.getElementById('ucsf_shuttle_trip_form_container');
 
         if (!response.hasOwnProperty('stops') && Modernizr.localstorage && localStorage.shuttle_stops) {
             try {
@@ -45,9 +45,10 @@ ucsf.shuttle = (function () {
         }
 
         var template = new Hogan.Template(
-            function(c,p,i){var _=this;_.b(i=i||"");_.b("<form action=\"javascript:ucsf.shuttle.plan()\"><h2>Trip Planner</h2><select name=\"begin\" id=\"ucsf_shuttle_starting_from\">");if(_.s(_.f("stops",c,p,1),c,p,0,130,206,"{{ }}")){_.rs(c,p,function(c,p,_){_.b("<option value=\"");if(_.s(_.f("id",c,p,1),c,p,0,152,171,"{{ }}")){_.rs(c,p,function(c,p,_){_.b(_.v(_.f("agencyId",c,p,0)));_.b("_");_.b(_.v(_.f("id",c,p,0)));});c.pop();}_.b("\">From ");_.b(_.v(_.f("stopName",c,p,0)));_.b("</option>");});c.pop();}_.b("</select>");if(!_.s(_.f("stops",c,p,1),c,p,1,0,0,"")){_.b("<p>Content could not be loaded.</p>");}_.b("<button type=\"button\" id=\"reverse_trip\" class=\"reverse_trip\">&uarr;&darr;</button><select name=\"end\" id=\"ucsf_shuttle_ending_at\">");if(_.s(_.f("stops",c,p,1),c,p,0,419,493,"{{ }}")){_.rs(c,p,function(c,p,_){_.b("<option value=\"");if(_.s(_.f("id",c,p,1),c,p,0,441,460,"{{ }}")){_.rs(c,p,function(c,p,_){_.b(_.v(_.f("agencyId",c,p,0)));_.b("_");_.b(_.v(_.f("id",c,p,0)));});c.pop();}_.b("\">To ");_.b(_.v(_.f("stopName",c,p,0)));_.b("</option>");});c.pop();}_.b("</select>");if(!_.s(_.f("stops",c,p,1),c,p,1,0,0,"")){_.b("<p>Content could not be loaded.</p>");}_.b("<fieldset disabled name=\"datetime\"><legend><select class=\"compact\" name=\"when\" onchange=\"form.datetime.disabled = value=='now'\"><option value=\"now\">Leave now</option><option value=\"depart\">Depart at</option><option value=\"arrive\">Arrive by</option></select></legend><select class=\"compact\" name=\"date\"><option value=\"today\">Today</option><option value=\"tomorrow\">Tomorrow</option></select><select class=\"compact\" name=\"time\"><option value=\"6:00am\">6:00am</option><option value=\"6:15am\">6:15am</option><option value=\"6:30am\">6:30am</option><option value=\"6:45am\">6:45am</option><option value=\"7:00am\">7:00am</option><option value=\"7:15am\">7:15am</option><option value=\"7:30am\">7:30am</option><option value=\"7:45am\">7:45am</option><option value=\"8:00am\">8:00am</option><option value=\"8:15am\">8:15am</option><option value=\"8:30am\">8:30am</option><option value=\"8:45am\">8:45am</option><option value=\"9:00am\">9:00am</option><option value=\"9:15am\">9:15am</option><option value=\"9:30am\">9:30am</option><option value=\"9:45am\">9:45am</option><option value=\"10:00am\">10:00am</option><option value=\"10:15am\">10:15am</option><option value=\"10:30am\">10:30am</option><option value=\"10:45am\">10:45am</option><option value=\"11:00am\">11:00am</option><option value=\"11:15am\">11:15am</option><option value=\"11:30am\">11:30am</option><option value=\"11:45am\">11:45am</option><option value=\"12:00pm\">12:00pm</option><option value=\"12:15pm\">12:15pm</option><option value=\"12:30pm\">12:30pm</option><option value=\"12:45pm\">12:45pm</option><option value=\"1:00pm\">1:00pm</option><option value=\"1:15pm\">1:15pm</option><option value=\"1:30pm\">1:30pm</option><option value=\"1:45pm\">1:45pm</option><option value=\"2:00pm\">2:00pm</option><option value=\"2:15pm\">2:15pm</option><option value=\"2:30pm\">2:30pm</option><option value=\"2:45pm\">2:45pm</option><option value=\"3:00pm\">3:00pm</option><option value=\"3:15pm\">3:15pm</option><option value=\"3:30pm\">3:30pm</option><option value=\"3:45pm\">3:45pm</option><option value=\"4:00pm\">4:00pm</option><option value=\"4:15pm\">4:15pm</option><option value=\"4:30pm\">4:30pm</option><option value=\"4:45pm\">4:45pm</option><option value=\"5:00pm\">5:00pm</option><option value=\"5:15pm\">5:15pm</option><option value=\"5:30pm\">5:30pm</option><option value=\"5:45pm\">5:45pm</option><option value=\"6:00pm\">6:00pm</option><option value=\"6:15pm\">6:15pm</option><option value=\"6:30pm\">6:30pm</option><option value=\"6:45pm\">6:45pm</option><option value=\"7:00pm\">7:00pm</option><option value=\"7:15pm\">7:15pm</option><option value=\"7:30pm\">7:30pm</option><option value=\"7:45pm\">7:45pm</option><option value=\"8:00pm\">8:00pm</option><option value=\"8:15pm\">8:15pm</option><option value=\"8:30pm\">8:30pm</option><option value=\"8:45pm\">8:45pm</option><option value=\"9:00pm\">9:00pm</option></select></fieldset><input type=\"submit\" name=\"route\" value=\"Route Trip\"  /></form>");return _.fl();}
+            function(c,p,i){var _=this;_.b(i=i||"");_.b("<form id=\"ucsf_shuttle_trip_form\" action=\"javascript:ucsf.shuttle.plan()\"><h2>Trip Planner</h2><select name=\"begin\" id=\"ucsf_shuttle_starting_from\">");if(_.s(_.f("stops",c,p,1),c,p,0,130,206,"{{ }}")){_.rs(c,p,function(c,p,_){_.b("<option value=\"");if(_.s(_.f("id",c,p,1),c,p,0,152,171,"{{ }}")){_.rs(c,p,function(c,p,_){_.b(_.v(_.f("agencyId",c,p,0)));_.b("_");_.b(_.v(_.f("id",c,p,0)));});c.pop();}_.b("\">From ");_.b(_.v(_.f("stopName",c,p,0)));_.b("</option>");});c.pop();}_.b("</select>");if(!_.s(_.f("stops",c,p,1),c,p,1,0,0,"")){_.b("<p>Content could not be loaded.</p>");}_.b("<button type=\"button\" id=\"reverse_trip\" class=\"reverse_trip\">&uarr;&darr;</button><select name=\"end\" id=\"ucsf_shuttle_ending_at\">");if(_.s(_.f("stops",c,p,1),c,p,0,419,493,"{{ }}")){_.rs(c,p,function(c,p,_){_.b("<option value=\"");if(_.s(_.f("id",c,p,1),c,p,0,441,460,"{{ }}")){_.rs(c,p,function(c,p,_){_.b(_.v(_.f("agencyId",c,p,0)));_.b("_");_.b(_.v(_.f("id",c,p,0)));});c.pop();}_.b("\">To ");_.b(_.v(_.f("stopName",c,p,0)));_.b("</option>");});c.pop();}_.b("</select>");if(!_.s(_.f("stops",c,p,1),c,p,1,0,0,"")){_.b("<p>Content could not be loaded.</p>");}_.b("<fieldset disabled name=\"datetime\"><legend><select class=\"compact\" name=\"when\" onchange=\"form.datetime.disabled = value=='now'\"><option value=\"now\">Leave now</option><option value=\"depart\">Depart at</option><option value=\"arrive\">Arrive by</option></select></legend><select class=\"compact\" name=\"date\"><option value=\"today\">Today</option><option value=\"tomorrow\">Tomorrow</option></select><select class=\"compact\" name=\"time\"><option value=\"6:00am\">6:00am</option><option value=\"6:15am\">6:15am</option><option value=\"6:30am\">6:30am</option><option value=\"6:45am\">6:45am</option><option value=\"7:00am\">7:00am</option><option value=\"7:15am\">7:15am</option><option value=\"7:30am\">7:30am</option><option value=\"7:45am\">7:45am</option><option value=\"8:00am\">8:00am</option><option value=\"8:15am\">8:15am</option><option value=\"8:30am\">8:30am</option><option value=\"8:45am\">8:45am</option><option value=\"9:00am\">9:00am</option><option value=\"9:15am\">9:15am</option><option value=\"9:30am\">9:30am</option><option value=\"9:45am\">9:45am</option><option value=\"10:00am\">10:00am</option><option value=\"10:15am\">10:15am</option><option value=\"10:30am\">10:30am</option><option value=\"10:45am\">10:45am</option><option value=\"11:00am\">11:00am</option><option value=\"11:15am\">11:15am</option><option value=\"11:30am\">11:30am</option><option value=\"11:45am\">11:45am</option><option value=\"12:00pm\">12:00pm</option><option value=\"12:15pm\">12:15pm</option><option value=\"12:30pm\">12:30pm</option><option value=\"12:45pm\">12:45pm</option><option value=\"1:00pm\">1:00pm</option><option value=\"1:15pm\">1:15pm</option><option value=\"1:30pm\">1:30pm</option><option value=\"1:45pm\">1:45pm</option><option value=\"2:00pm\">2:00pm</option><option value=\"2:15pm\">2:15pm</option><option value=\"2:30pm\">2:30pm</option><option value=\"2:45pm\">2:45pm</option><option value=\"3:00pm\">3:00pm</option><option value=\"3:15pm\">3:15pm</option><option value=\"3:30pm\">3:30pm</option><option value=\"3:45pm\">3:45pm</option><option value=\"4:00pm\">4:00pm</option><option value=\"4:15pm\">4:15pm</option><option value=\"4:30pm\">4:30pm</option><option value=\"4:45pm\">4:45pm</option><option value=\"5:00pm\">5:00pm</option><option value=\"5:15pm\">5:15pm</option><option value=\"5:30pm\">5:30pm</option><option value=\"5:45pm\">5:45pm</option><option value=\"6:00pm\">6:00pm</option><option value=\"6:15pm\">6:15pm</option><option value=\"6:30pm\">6:30pm</option><option value=\"6:45pm\">6:45pm</option><option value=\"7:00pm\">7:00pm</option><option value=\"7:15pm\">7:15pm</option><option value=\"7:30pm\">7:30pm</option><option value=\"7:45pm\">7:45pm</option><option value=\"8:00pm\">8:00pm</option><option value=\"8:15pm\">8:15pm</option><option value=\"8:30pm\">8:30pm</option><option value=\"8:45pm\">8:45pm</option><option value=\"9:00pm\">9:00pm</option></select></fieldset><input type=\"submit\" name=\"route\" value=\"Route Trip\"  /></form>");return _.fl();}
         );
-        form.innerHTML = template.render(response);
+        formContainer.innerHTML = template.render(response);
+
         var start = document.getElementById('ucsf_shuttle_starting_from'),
             end = document.getElementById('ucsf_shuttle_ending_at');
 
@@ -142,16 +143,23 @@ ucsf.shuttle = (function () {
     };
 
     me.plan = function () {
+        var form = document.getElementById('ucsf_shuttle_trip_form');
+        var options = {
+            apikey:'c631ef46e918c82cf81ef4869f0029d4',
+            fromPlace:form.begin.value,
+            toPlace:form.end.value
+        };
+
+        //TODO: time should default to something close to now
         //TODO: send time/date to UCSF.Shuttle.plan()
         //TODO: send arriveBy to UCSF.Shuttle.plan()
-        UCSF.Shuttle.plan(
-            {
-                apikey:'c631ef46e918c82cf81ef4869f0029d4',
-                fromPlace:document.getElementById('ucsf_shuttle_starting_from').value,
-                toPlace:document.getElementById('ucsf_shuttle_ending_at').value
-            },
-            ucsf.shuttle.renderTrip
-        );
+
+        if (form.when.value !== "now") {
+            options.arriveBy = form.when.value==="arrive";
+            options.time = form.time.value;
+            options.date = form.date.value;
+        }
+        UCSF.Shuttle.plan(options, ucsf.shuttle.renderTrip);
         me.save();
     };
 
@@ -161,13 +169,14 @@ ucsf.shuttle = (function () {
 Modernizr.load({
     load: 'http://apis.ucsf.edu/static/UCSF.Shuttle.js',
     callback: function () {
-        var form = document.getElementById('ucsf_shuttle_trip_form');
-        if (form) {
+        var formContainer = document.getElementById('ucsf_shuttle_trip_form_container');
+        if (formContainer) {
             UCSF.Shuttle.stops({apikey:'c631ef46e918c82cf81ef4869f0029d4'}, ucsf.shuttle.renderForm);
         }
 
     }
 });
 
-//TODO: scroll-to results when they are done
+//TODO: scroll-to results when they are done (directory too)
+//TODO: back button (directory too)
 
