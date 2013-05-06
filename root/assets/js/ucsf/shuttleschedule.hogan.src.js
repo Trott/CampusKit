ee046cbba3f104cd786cf83e4e85247b13b0d4de
Rich Trott <rtrott@gmail.com>
2013-05-06 13:12:31 -0700

    insert code for schedule times

--- root/assets/js/ucsf/shuttleschedule.hogan.src.js
+++ root/assets/js/ucsf/shuttleschedule.hogan.src.js
@@ -2,10 +2,12 @@
 var hogan = require("hogan.js");
 
 // construct template string
-var template = '<h2>{{#route}}{{routeShortName}} {{/route}}Shuttle Schedule</h2>';
-	template += '<ol>';
+var template = '{{#route}}';
+    template += '<h2>{{routeShortName}} Shuttle Schedule</h2>';
+	template += '<ol id="ucsf-schedule-container" data-routeId="{{#id}}{{id}}{{/id}}">';
+    template += '{{/route}}';
     template += '{{#stops}}';
-    template += '<li><a href="#">{{stopName}}</a></li>';
+    template += '<li><a href="javascript:UCSF.Shuttle.times({ {{#id}}stopId:\'{{id}}\',{{/id}} routeId:document.getElementById(\'ucsf-schedule-container\').getAttribute(\'data-routeId\'), startTime:Date.now(), endTime:new Date().setHours(23,59,59,999) },function(res) {console.dir(res);})">{{stopName}}</a></li>';
     template += '{{/stops}}';
     template += '</ol>';
     template += '{{^stops}}';
