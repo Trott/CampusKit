d6f2fcf118da2246564e5d5bd35718eee043ca15
Rich Trott <rtrott@gmail.com>
2013-05-07 16:19:01 -0700

    include apikey

--- root/assets/js/ucsf/shuttleschedulemenu.hogan.src.js
+++ root/assets/js/ucsf/shuttleschedulemenu.hogan.src.js
@@ -7,7 +7,7 @@ var template = '{{#route}}';
 	template += '<ol id="ucsf-schedule-container" data-routeId="{{#id}}{{id}}{{/id}}">';
     template += '{{/route}}';
     template += '{{#stops}}';
-    template += '<li><a href="#" data-stopId="{{#id}}{{id}}{{/id}}" onclick="ucsf.shuttle.renderSchedule.target=this;ucsf.shuttle.renderSchedule.startTime=new Date().setHours(0,0,0,0);UCSF.Shuttle.times({ {{#id}}stopId:\'{{id}}\',{{/id}}routeId:document.getElementById(\'ucsf-schedule-container\').getAttribute(\'data-routeId\'),startTime:ucsf.shuttle.renderSchedule.startTime,endTime:ucsf.shuttle.renderSchedule.startTime+86399999},ucsf.shuttle.renderSchedule);return false">{{stopName}}</a></li>';
+    template += '<li><a href="#" data-stopId="{{#id}}{{id}}{{/id}}" onclick="ucsf.shuttle.renderSchedule.target=this;ucsf.shuttle.renderSchedule.startTime=new Date().setHours(0,0,0,0);UCSF.Shuttle.times({apikey:\'c631ef46e918c82cf81ef4869f0029d4\',{{#id}}stopId:\'{{id}}\',{{/id}}routeId:document.getElementById(\'ucsf-schedule-container\').getAttribute(\'data-routeId\'),startTime:ucsf.shuttle.renderSchedule.startTime,endTime:ucsf.shuttle.renderSchedule.startTime+86399999},ucsf.shuttle.renderSchedule);return false">{{stopName}}</a></li>';
     template += '{{/stops}}';
     template += '</ol>';
     template += '{{^stops}}';
