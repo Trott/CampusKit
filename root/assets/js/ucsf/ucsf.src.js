250eeb7d991c09089de537d53c6ba7eba5ccc306
Rich Trott <rtrott@gmail.com>
2013-05-10 13:34:27 -0700

    refactor

--- root/assets/js/ucsf/ucsf.src.js
+++ root/assets/js/ucsf/ucsf.src.js
@@ -1,2 +1,8 @@
 var ucsf = ucsf || {};
+
+ucsf.spin = function (el) {
+	el.innerHTML = '<div class="spinner"></div>';
+	document.body.setAttribute("style", "background:rgba(0,0,0,0.5)");
+};
+
 FastClick.attach(document.body);
