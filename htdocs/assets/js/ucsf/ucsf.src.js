c78c3c349445305aa22738c528557b4e2cfb9792
Rich Trott <rtrott@gmail.com>
2013-05-14 14:30:09 -0700

    check setAttribute()

--- htdocs/assets/js/ucsf/ucsf.src.js
+++ htdocs/assets/js/ucsf/ucsf.src.js
@@ -2,7 +2,10 @@ var ucsf = ucsf || {};
 
 ucsf.spin = function (el) {
 	el.innerHTML = '<div class="spinner"></div>';
-	document.body.setAttribute("style", "background:rgba(0,0,0,0.5)");
+	// Check that setAttribute() exists so that IE doesn't blow up
+	if ('setAttribute' in document.body) {
+		document.body.setAttribute("style", "background:rgba(0,0,0,0.5)");
+	}
 };
 
 FastClick.attach(document.body);
