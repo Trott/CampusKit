2b66d57fe99fb3f57ef4671fb5dc707b231ae10a
Rich Trott <rtrott@gmail.com>
2013-05-14 14:36:14 -0700

    comment

--- htdocs/assets/js/ucsf/ucsf.src.js
+++ htdocs/assets/js/ucsf/ucsf.src.js
@@ -2,7 +2,7 @@ var ucsf = ucsf || {};
 
 ucsf.spin = function (el) {
 	el.innerHTML = '<div class="spinner"></div>';
-	// Check that setAttribute() exists so that IE doesn't blow up
+	// Check that setAttribute() exists so that IE7 doesn't blow up
 	if ('setAttribute' in document.body) {
 		document.body.setAttribute("style", "background:rgba(0,0,0,0.5)");
 	}
