ee18c2c61b9e88259b55422dd6a8e70abc4e877f
Rich Trott <rtrott@gmail.com>
2013-05-14 14:43:48 -0700

    :

--- htdocs/assets/js/ucsf/shuttle.src.js
+++ htdocs/assets/js/ucsf/shuttle.src.js
@@ -316,3 +316,4 @@ Modernizr.load({
 //TODO: needs to work in IE. Maybe problem is that addEventListener works in IE9 but FastClick does not?
 //TODO: make sure all the old URLs work for schedules, or at least get redirected reasonably
 //TODO: orthosurg and fitness tests and fixes
+//TODO: center div spinners in progress elements
--- htdocs/assets/js/ucsf/ucsf.src.js
+++ htdocs/assets/js/ucsf/ucsf.src.js
@@ -3,10 +3,10 @@ var ucsf = ucsf || {};
 ucsf.spin = function (el) {
 	el.innerHTML = '<div class="spinner"></div>';
 	// Check that setAttribute() exists so that IE7 doesn't blow up
-	if ('setAttribute' in document.body) {
-		document.body.setAttribute("style", "background:rgba(0,0,0,0.5)");
-	}
-	return true;
+	// if ('setAttribute' in document.body) {
+    //  document.body.setAttribute("style", "background:rgba(0,0,0,0.5)");
+	// }
+	// return true;
 };
 
 FastClick.attach(document.body);
