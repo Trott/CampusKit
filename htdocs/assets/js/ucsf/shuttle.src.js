702fc45e7f2178aa444f2cf7b77668160ab7611b
Rich Trott <rtrott@gmail.com>
2013-05-15 22:36:33 -0700

    fix stupid issue with FastClick + AppCache + iOS Safari + airplane mode means some links just do nothing when they should tell the user that they are offline

--- htdocs/assets/js/ucsf/shuttle.src.js
+++ htdocs/assets/js/ucsf/shuttle.src.js
@@ -316,6 +316,5 @@ Modernizr.load({
 //TODO: make sure all the old URLs work for schedules, or at least get redirected reasonably
 //TODO: center div spinners in progress elements
 //TODO: kill PHP/JS redirect
-//TODO: iOS: FastClick? causes links that are not appacached to not respond at all in Safari when in Airplane mode
 //TODO: add ucsf-list-arrow.png to appacache
 //TODO: add separator to appcache or remove separator stuff altogether
--- htdocs/assets/js/ucsf/ucsf.src.js
+++ htdocs/assets/js/ucsf/ucsf.src.js
@@ -1,18 +1,27 @@
 FastClick.attach(document.body);
 
-// FastClick + offline appcache + entities not in offline appcache =
+// FastClick + offline appcache + links to pages not in offline appcache + iOS Safari =
 //  links that do nothing when they should report a "you're offline!" error
 // If we convert everything to SPA, we'll still need something like this
 //  for offsite links.
 
 if (window.addEventListener) {
     window.addEventListener(
-        'unload',
-        function() {
-            if (window.navigator.onLine && window.navigator.onLine === false) {
-                window.alert('Some content may not be available because you are offline.');
+        'click',
+        function () {
+            if (window.navigator.onLine !== void 0 && window.navigator.onLine === false) {
+                // Wait 100 ms to let appcached content load so we don't throw the alert
+                // if either the browser knows to put up a "you're offline!" page or if
+                // the page is in the appcache.
+                window.setTimeout(
+                    function () {
+                        window.alert('Some content may not be available because you are offline.');
+                    },
+                    100
+                );
             }
         },
         false
     );
 }
+
