88c3ab2556014f7323483c6ef5dfe1c1beafab93
Rich Trott <rtrott@gmail.com>
2013-05-15 21:30:44 -0700

    experiment with unload

--- htdocs/assets/js/ucsf/ucsf.src.js
+++ htdocs/assets/js/ucsf/ucsf.src.js
@@ -1,1 +1,18 @@
 FastClick.attach(document.body);
+
+// FastClick + offline appcache + entities not in offline appcache =
+//  links that do nothing when they should report a "you're offline!" error
+// If we convert everything to SPA, we'll still need something like this
+//  for offsite links.
+
+if (window.addEventListener) {
+    window.addEventListener(
+        'unload',
+        function() {
+            if (window.navigator.onLine && window.navigator.onLine === false) {
+                window.alert('Some content may not be available because you are offline.');
+            }
+        },
+        false
+    );
+}
