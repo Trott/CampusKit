8dabe0f51a5440600ac9197966df64b6b35f218b
Rich Trott <rtrott@gmail.com>
2013-05-24 17:18:27 -0700

    no quick flash of unstyled content

--- htdocs/assets/js/ucsf/free_food.src.js
+++ htdocs/assets/js/ucsf/free_food.src.js
@@ -3,23 +3,30 @@ var ucsf = ucsf || {};
 ucsf.FreeFood = {
     Controller: function (scope) {
         scope.isLoading = true;
+        scope.message = "";
         scope.events = [];
 
         Modernizr.load({
             load: "http://apis.ucsf.edu/jsapi?free_food",
             callback: function () {
-                var apikey='c631ef46e918c82cf81ef4869f0029d4';
-                UCSF.FreeFood.events(
-                    {apikey:apikey},
-                    function(result) {
+                var apikey='c631ef46e918c82cf81ef4869f0029d4',
+                    innerCallback = function (result) {
                         scope.$apply(function () {
                             scope.events = result.events || [];
                             scope.isLoading = false;
+                            if (! scope.events.length) {
+                                scope.message = "No listings found. Please try again later.";
+                            }
                         });
-                    }
+                    };
+                UCSF.FreeFood.events(
+                    {apikey:apikey},
+                    innerCallback,
+                    innerCallback
                 );
            }
         });
+
     }
 };
 
