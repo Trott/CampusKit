718bc15e4eaab148971d636fd4927ed5f5e5e52f
Rich Trott <rtrott@gmail.com>
2013-06-12 08:21:18 -0700

    Working in Android

--- src/js/modules/shuttle/shuttle.src.js
+++ src/js/modules/shuttle/shuttle.src.js
@@ -11,7 +11,7 @@
     }])
     .controller(
         'planController',
-        ['$scope', '$filter', '$http', '$location', '$routeParams', function ($scope, $filter, $http, $location, $routeParams) {
+        ['$scope', '$filter', '$location', '$routeParams', function ($scope, $filter, $location, $routeParams) {
             var apikey = 'c631ef46e918c82cf81ef4869f0029d4',
                 xhrFunction,
                 xhrOptions;
@@ -37,82 +37,74 @@
 
                 $scope.isLoading = true;
 
-                // UCSF is from a script only loaded in IE9 and below.
-                // It knows to use XDR if there's no XHR.
-                if (typeof UCSF !== 'undefined') {
-                    xhrFunction = UCSF.Shuttle.plan;
-                    xhrOptions = planXhrParams;
-                } else {
-                    xhrFunction = $http;
-                    xhrOptions = {
-                        method: 'GET',
-                        url: 'http://apis.ucsf.edu/shuttle/plan',
-                        params: planXhrParams
-                    };
-                }
+                UCSF.Shuttle.plan(
+                    planXhrParams,
+                    function (data) {
+                        $scope.isLoading = false;
+                        $scope.planLoaded = true;
+                        var plan = data.plan || {};
+                        if (plan.itineraries) {
+                            // Used to check that the plan is within four hours of the target time.
+                            var datestamp = plan.date ? plan.date : Date.now();
+                            var fourHours = 4 * 60 * 60 * 1000;
+                            var index = 1;
+                            var itinerariesCount = plan.itineraries.length;
+                            var removalQueue = [];
+                            var massagedLeg;
+                            var tripSteps;
+                            var thisItinerary;
+                            // Decrement with while rather than increment with for so that splice() doesn't mess up the loop
+                            for (var i = 0; i < itinerariesCount; i++) {
+                                thisItinerary = plan.itineraries[i];
+                                // Only use itineraries that are less than 2 hours (e.g., not overnight)
+                                // This check should probably be happening on the server side.
+                                // Then check that the time is within four hours of what was chosen
+                                // so the user doesn't get a trip for the next day.
+                                if ((thisItinerary.duration < 2 * 60 * 60 * 1000) && (thisItinerary.endTime >= datestamp - fourHours) && (thisItinerary.endTime <= datestamp + fourHours)) {
+                                    thisItinerary.index = index;
+                                    index++;
+                                    thisItinerary.startTimeFormatted = $filter('date')(thisItinerary.startTime, 'shortTime');
+                                    thisItinerary.endTimeFormatted = $filter('date')(thisItinerary.endTime, 'shortTime');
+                                    thisItinerary.durationFormatted = Math.round(thisItinerary.duration / (60 * 1000));
+                                    if (thisItinerary.hasOwnProperty('legs')) {
+                                        tripSteps = thisItinerary.legs.length;
+                                        for (var j = 0; j < tripSteps; j++) {
+                                            massagedLeg = {};
+                                            massagedLeg.toName = thisItinerary.legs[j].to.name;
 
-                xhrFunction(xhrOptions).success(function (data) {
-                    $scope.isLoading = false;
-                    $scope.planLoaded = true;
-                    var plan = data.plan || {};
-                    if (plan.itineraries) {
-                        // Used to check that the plan is within four hours of the target time.
-                        var datestamp = plan.date ? plan.date : Date.now();
-                        var fourHours = 4 * 60 * 60 * 1000;
-                        var index = 1;
-                        var itinerariesCount = plan.itineraries.length;
-                        var removalQueue = [];
-                        var massagedLeg;
-                        var tripSteps;
-                        var thisItinerary;
-                        // Decrement with while rather than increment with for so that splice() doesn't mess up the loop
-                        for (var i = 0; i < itinerariesCount; i++) {
-                            thisItinerary = plan.itineraries[i];
-                            // Only use itineraries that are less than 2 hours (e.g., not overnight)
-                            // This check should probably be happening on the server side.
-                            // Then check that the time is within four hours of what was chosen
-                            // so the user doesn't get a trip for the next day.
-                            if ((thisItinerary.duration < 2 * 60 * 60 * 1000) && (thisItinerary.endTime >= datestamp - fourHours) && (thisItinerary.endTime <= datestamp + fourHours)) {
-                                thisItinerary.index = index;
-                                index++;
-                                thisItinerary.startTimeFormatted = $filter('date')(thisItinerary.startTime, 'shortTime');
-                                thisItinerary.endTimeFormatted = $filter('date')(thisItinerary.endTime, 'shortTime');
-                                thisItinerary.durationFormatted = Math.round(thisItinerary.duration / (60 * 1000));
-                                if (thisItinerary.hasOwnProperty('legs')) {
-                                    tripSteps = thisItinerary.legs.length;
-                                    for (var j = 0; j < tripSteps; j++) {
-                                        massagedLeg = {};
-                                        massagedLeg.toName = thisItinerary.legs[j].to.name;
+                                            if (thisItinerary.legs[j].mode === 'BUS') {
+                                                massagedLeg.fromName = thisItinerary.legs[j].from.name;
+                                                massagedLeg.route = thisItinerary.legs[j].route;
+                                                massagedLeg.routeId = thisItinerary.legs[j].routeId;
+                                                massagedLeg.startTime = $filter('date')(thisItinerary.legs[j].startTime, 'shortTime');
+                                                massagedLeg.endTime = $filter('date')(thisItinerary.legs[j].endTime, 'shortTime');
+                                                thisItinerary.legs[j].bus = massagedLeg;
+                                            }
 
-                                        if (thisItinerary.legs[j].mode === 'BUS') {
-                                            massagedLeg.fromName = thisItinerary.legs[j].from.name;
-                                            massagedLeg.route = thisItinerary.legs[j].route;
-                                            massagedLeg.routeId = thisItinerary.legs[j].routeId;
-                                            massagedLeg.startTime = $filter('date')(thisItinerary.legs[j].startTime, 'shortTime');
-                                            massagedLeg.endTime = $filter('date')(thisItinerary.legs[j].endTime, 'shortTime');
-                                            thisItinerary.legs[j].bus = massagedLeg;
-                                        }
-
-                                        if (thisItinerary.legs[j].mode === 'WALK') {
-                                            thisItinerary.legs[j].walk = massagedLeg;
+                                            if (thisItinerary.legs[j].mode === 'WALK') {
+                                                thisItinerary.legs[j].walk = massagedLeg;
+                                            }
                                         }
                                     }
+                                } else {
+                                    removalQueue.push(i);
                                 }
-                            } else {
-                                removalQueue.push(i);
                             }
+                            // Start from end so splice() doesn't mess up
+                            i = removalQueue.length;
+                            while (i--) {
+                                plan.itineraries.splice(removalQueue[i], 1);
+                            }
+                            $scope.itineraries = plan.itineraries;
                         }
-                        // Start from end so splice() doesn't mess up
-                        i = removalQueue.length;
-                        while (i--) {
-                            plan.itineraries.splice(removalQueue[i], 1);
-                        }
-                        $scope.itineraries = plan.itineraries;
-                    }
+                        $scope.$apply();
+                    },
                     //TODO: resultsElement.scrollIntoView();
-                }).error(function () {
+                    function () {
                     //TODO: uh, yeah, this
-                });
+                        $scope.$apply();
+                    }
+                );
             }
 
             if (! $scope.time) {
@@ -132,52 +124,43 @@
             $scope.planLoaded = false;
             $scope.planLoadError = false;
 
+            UCSF.Shuttle.stops(
+                {apikey: apikey},
+                function (data) {
+                    $scope.isLoading = false;
+                    if (data.stops) {
+                        $scope.stops = $filter('orderBy')(data.stops, 'stopName');
 
-            // UCSF is from a script only loaded in IE9 and below.
-            // It knows to use XDR if there's no XHR.
-            if (typeof UCSF !== 'undefined') {
-                xhrFunction = UCSF.Shuttle.stops;
-                xhrOptions = {apikey: apikey};
-            } else {
-                xhrFunction = $http;
-                xhrOptions = {
-                    method: 'GET',
-                    url: 'http://apis.ucsf.edu/shuttle/stops',
-                    params: {apikey: apikey}
-                };
-            }
-
-            xhrFunction(xhrOptions).success(function (data) {
-                $scope.isLoading = false;
-                if (data.stops) {
-                    $scope.stops = $filter('orderBy')(data.stops, 'stopName');
-
-                    $scope.begin = $filter('filter')($scope.stops,
-                        function (elem) {
-                            return elem.stopName === 'Parnassus Campus';
+                        $scope.begin = $filter('filter')($scope.stops,
+                            function (elem) {
+                                return elem.stopName === 'Parnassus Campus';
+                            }
+                            )[0];
+                        $scope.end = $filter('filter')($scope.stops,
+                            function (elem) {
+                                return elem.stopName === 'Mission Bay Campus';
+                            }
+                            )[0];
+                        if (Modernizr.localstorage) {
+                            if (localStorage.shuttleStart) {
+                                $scope.begin = $scope.stops[localStorage.shuttleStart] || $scope.begin;
+                            }
+                            if (localStorage.shuttleEnd) {
+                                $scope.end = $scope.stops[localStorage.shuttleEnd] || $scope.end;
+                            }
                         }
-                        )[0];
-                    $scope.end = $filter('filter')($scope.stops,
-                        function (elem) {
-                            return elem.stopName === 'Mission Bay Campus';
-                        }
-                        )[0];
-                    if (Modernizr.localstorage) {
-                        if (localStorage.shuttleStart) {
-                            $scope.begin = $scope.stops[localStorage.shuttleStart] || $scope.begin;
-                        }
-                        if (localStorage.shuttleEnd) {
-                            $scope.end = $scope.stops[localStorage.shuttleEnd] || $scope.end;
-                        }
+                        $scope.stopsLoaded = true;
+                    } else {
+                        $scope.stopsLoadError = true;
                     }
-                    $scope.stopsLoaded = true;
-                } else {
+                    $scope.$apply();
+                },
+                function () {
+                    $scope.isLoading = false;
                     $scope.stopsLoadError = true;
+                    $scope.$apply();
                 }
-            }).error(function () {
-                $scope.isLoading = false;
-                $scope.stopsLoadError = true;
-            });
+            );
 
             $scope.plan = function () {
                 var fromPlace = $scope.begin.id.agencyId + '_' + $scope.begin.id.id,
