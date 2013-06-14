f0773679fad825f9e50da06c9224b5725ff1f82c
Rich Trott <rtrott@gmail.com>
2013-06-14 10:22:29 -0700

    JS grunt tasks are generalized

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,24 @@
 /*global module:false*/
 module.exports = function(grunt) {
 
+    var site = 'sites/' + (grunt.option('site') || 'demo');
+
     // Project configuration.
     grunt.initConfig({
         bower: {
             install: {}
         },
 
-        clean: ['htdocs/assets/js', 'tmp'],
+        clean: ['htdocs/', 'tmp'],
 
         concat: {
             full: {
-                src: ['tmp/fastclick.min.js', 'src/js/external/modernizr.js', 'tmp/ucsf.partial.min.js'],
-                dest: 'htdocs/assets/js/ucsf.js'
+                src: ['tmp/fastclick.min.js', 'src/js/external/modernizr.js', 'tmp/campuskit.partial.min.js'],
+                dest: 'htdocs/assets/js/campuskit.js'
             },
             partial: {
-                src: ['src/js/ucsf.src.js', 'src/js/analytics.src.js'],
-                dest: 'tmp/ucsf.partial.js'
+                src: ['src/js/campuskit.src.js', site + '/js/*.src.js'],
+                dest: 'tmp/campuskit.partial.js'
             }
         },
 
@@ -49,8 +51,8 @@ module.exports = function(grunt) {
                 Hogan: true,
                 ucsf: true
             },
-            beforeconcat: ['src/js/modules/*/*.src.js'],
-            afterconcat: ['tmp/ucsf.partial.js'],
+            beforeconcat: [site + '/js/modules/*/*.src.js'],
+            afterconcat: ['tmp/campuskit.partial.js'],
             gruntfile: ['Gruntfile.js']
         },
 
@@ -89,40 +91,22 @@ module.exports = function(grunt) {
                     'tmp/fastclick.min.js': ['lib/fastclick/fastclick.js']
                 }
             },
-            ucsf: {
+            campuskit: {
                 files: {
-                    'tmp/ucsf.partial.min.js': ['tmp/ucsf.partial.js']
+                    'tmp/campuskit.partial.min.js': ['tmp/campuskit.partial.js']
                 }
             },
-            profile: {
-                files: {
-                    'htdocs/assets/js/profile.js': ['src/js/modules/research/profile.src.js']
-                }
-            },
-            shuttle: {
-                files: {
-                    'htdocs/assets/js/shuttle.js': ['lib/hogan/web/builds/2.0.0/template-2.0.0.js','src/js/modules/shuttle/shuttle.src.js']
-                }
-            },
-            directory: {
-                files: {
-                    'htdocs/assets/js/directory.js': ['lib/hogan/web/builds/2.0.0/template-2.0.0.js','src/js/modules/directory/directory.src.js']
-                }
-            },
-            news: {
-                files: {
-                    'htdocs/assets/js/news.js': ['lib/hogan/web/builds/2.0.0/template-2.0.0.js','src/js/modules/news/news.src.js']
-                }
-            },
-            maps: {
-                files: {
-                    'htdocs/assets/js/maps.js': ['lib/hogan/web/builds/2.0.0/template-2.0.0.js','src/js/modules/maps/maps.src.js']
-                }
-            },
-            free_food: {
-                files: {
-                    'htdocs/assets/js/free_food.js': ['src/js/modules/free_food/free_food.src.js']
-                }
+            modules: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: site + '/js/modules/',
+                        src: ['**/*.js'],
+                        dest: 'htdocs/assets/js/modules/',
+                        ext: '.js',
+                        flatten: true
+                    }
+                ]
             }
         },
 
