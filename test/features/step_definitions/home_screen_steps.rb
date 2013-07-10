4c190f5c7fa9e45320d9d3101ab599d9390fd78b
Rich Trott <rtrott@gmail.com>
2013-07-09 23:01:27 -0700

    update home screen tests

--- test/features/step_definitions/home_screen_steps.rb
+++ test/features/step_definitions/home_screen_steps.rb
@@ -1,13 +1,12 @@
-Given /^I am on the home page$/ do
+Given(/^I am on the home page$/) do
   visit "/"
 end
 
-Then /^I should see the home page$/ do
-  should have_selector("body.front")
-  should have_selector("div#main_menu")
-  should have_selector("ol.menu.front")
+Then(/^I should see the home page$/) do
+  should have_selector("ol.front")
+  should have_selector("div#main")
 end
 
-Given /^I am on the Native iOS home page$/ do
+Given(/^I am on the Native iOS home page$/) do
   visit "/nativeios.html"
 end
