2ab126f2e0c5069f2368bc24329f66d6bd1de62c
Rich Trott <rtrott@gmail.com>
2013-07-12 10:39:11 -0700

    events

--- sites/ucsf/js/modules/news/news.src.js
+++ sites/ucsf/js/modules/news/news.src.js
@@ -1,73 +1,23 @@
 (function () {
     'use strict';
-    angular.module('news', [])
+    angular.module('news', ['rssReader'])
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider
         .when('/news', {templateUrl: 'partials/news/mainMenu.html', controller: 'newsController'});
     }])
     .controller(
         'newsController',
-        ['$scope', function ($scope) {
-            var loadFromStorage = function (storageId) {
-                var stored;
-                if (Modernizr.localstorage) {
-                    stored = window.localStorage.getItem(storageId);
-                    if (stored !== null) {
-                        return JSON.parse(stored);
-                    }
+        ['$scope', 'rssReaderService', function ($scope, rssReaderService) {
+            var promise = rssReaderService.getRssData(
+                'http://feeds.feedburner.com/UCSF_News',
+                $scope,
+                {localStorageKey: 'feed_ucsf_news'}
+            );
+            promise.then(
+                function(feed) {
+                    $scope.feed = feed;
                 }
-                return {};
-            };
-
-            if (! navigator.onLine) {
-                $scope.feed = loadFromStorage('ucsf_news_feed').feed;
-                $scope.apply();
-            } else {
-                Modernizr.load([{load:'http://www.google.com/jsapi', callback: function() {
-                    google.load("feeds", "1",
-                        {
-                            nocss: true,
-                            callback: function () {
-                                var feed = new google.feeds.Feed('http://feeds.feedburner.com/UCSF_News');
-
-                                feed.load(function (result) {
-                                    var i,
-                                    thisEntry,
-                                    dateTime,
-                                    dateObject,
-                                    hours,
-                                    minutes,
-                                    designation,
-                                    content = {};
-
-                                    if (! result.error) {
-                                        content = {
-                                            "feed": this.feed
-                                        };
-                                        for (i = 0; i < content.feed.entries.length; i = i + 1) {
-                                            thisEntry = content.feed.entries[i];
-                                            dateTime = {};
-                                            dateObject = new Date(thisEntry.publishedDate);
-                                            dateTime.date = dateObject.toLocaleDateString();
-                                            thisEntry.dateTime = dateTime;
-                                        }
-
-                                        if (Modernizr.localstorage) {
-                                            window.localStorage.setItem('feed_ucsf_news', JSON.stringify(content));
-                                        }
-                                        $scope.feed = content.feed;
-                                        $scope.$apply();
-                                    } else {
-                                        $scope.feed = loadFromStorage('feed_ucsf_news').feed;
-                                        $scope.$apply();
-                                    }
-                                });
-
-                            }
-                        }
-                    );
-                }}]);
-            }
+            );
         }]
     );
 
