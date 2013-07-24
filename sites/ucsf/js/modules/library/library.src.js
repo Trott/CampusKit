a185ae9b8c57c0f006dea7f13d0c8e24378c7cc9
Rich Trott <rtrott@gmail.com>
2013-07-24 10:41:48 -0700

    begin modifications for timetable via API

--- sites/ucsf/js/modules/library/library.src.js
+++ sites/ucsf/js/modules/library/library.src.js
@@ -1,10 +1,37 @@
 (function () {
     'use strict';
+    var apikey = 'c631ef46e918c82cf81ef4869f0029d4';
+
     angular.module('library', [])
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider
         .when('/library', {templateUrl: 'partials/library/mainMenu.html'})
-        .when('/library/locations', {templateUrl: 'partials/library/locations.html'})
+        .when('/library/locations', {controller: 'locationsLibraryController', templateUrl: 'partials/library/locations.html'})
         .when('/library/help', {templateUrl: 'partials/library/help.html'});
+    }])
+    .controller('locationsLibraryController', ['$scope', function ($scope) {
+        $scope.loading = true;
+        $scope.loaded = false;
+        $scope.loadError = false;
+
+        if (window.UCSF && window.UCSF.Library && typeof window.UCSF.Library.hours === 'function') {
+            window.UCSF.Library.hours(
+                {apikey:apikey}, 
+                function (data) {
+                    $scope.$apply(function () {
+                        $scope.loading = false;
+                        $scope.loaded = true;
+                        $scope.hours = data.locations;
+                    });
+                },
+                function () {
+                    $scope.$apply(function () {
+                        $scope.loading = false;
+                        $scope.loadError = true;
+                    });
+                }
+            );
+
+        }
     }]);
 }());
