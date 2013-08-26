87253c4fcc793e209a576d2d468f60d2053d698f
Rich Trott <rtrott@gmail.com>
2013-08-25 23:01:16 -0700

    stub spec

---
+++ sites/ucsf/test/unit/mainSpec.js
@@ -0,0 +1,15 @@
+describe('UCSF controllers', function () {
+
+    describe('BackButtonController', function (){
+
+        it('should set hideBackButton to true if path is /', function () {
+            var scope = {};
+            var location = {
+                path: '/'
+            };
+            //var ctrl = new BackButtonController(scope, location);
+
+            //expect(scope.hideBackButton).toBe(true);
+        });
+    });
+});
