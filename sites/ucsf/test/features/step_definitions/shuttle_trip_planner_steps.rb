1e30d03f8d9138ea0b2dac2451e38492ae4d731f
Rich Trott <rtrott@gmail.com>
2013-10-27 13:36:41 -0700

    formatting

--- sites/ucsf/test/features/step_definitions/shuttle_trip_planner_steps.rb
+++ sites/ucsf/test/features/step_definitions/shuttle_trip_planner_steps.rb
@@ -1,17 +1,17 @@
 # encoding: utf-8
 
 Given(/^my localStorage is empty$/) do
-	visit "/"
-	execute_script('localStorage.clear()')
-	visit "http://google.com/"
+    visit "/"
+    execute_script('localStorage.clear()')
+    visit "http://google.com/"
 end
 
 Given(/I visit the Shuttle Trip Planner page/) do
-	visit "#/shuttle/planner"
+    visit "#/shuttle/planner"
 end
 
 Then(/^I should see "(.*?)" selected$/) do |value|
-	find(:xpath, "//option[text() = '#{value}']").should be_selected
+    find(:xpath, "//option[text() = '#{value}']").should be_selected
 end
 
 Given(/^I select a route "(.*?)"$/) do |value|
@@ -19,11 +19,11 @@ Given(/^I select a route "(.*?)"$/) do |value|
 end
 
 Then(/^I route the trip$/) do
-	click_button('Route Trip')
+    click_button('Route Trip')
 end
 
 And(/^I reverse the trip$/) do
-	click_button('↑↓')
+    click_button('↑↓')
 end
 
 Then(/^I should see "(.*?)"$/) do |text|
@@ -31,9 +31,9 @@ Then(/^I should see "(.*?)"$/) do |text|
 end
 
 Then(/^"(.*?)" should not be disabled$/) do |arg1|
-  find(:xpath, "//fieldset[@name='datetime']")[:disabled].should_not eq "true"
+    find(:xpath, "//fieldset[@name='datetime']")[:disabled].should_not eq "true"
 end
 
 Given(/^I select "(.*?)" for "(.*?)"$/) do |value, name|
-  select(value, :from => name)
+    select(value, :from => name)
 end
