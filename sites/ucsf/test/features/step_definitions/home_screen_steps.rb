ff68f109898521fd31ee2ae9e2937231d20b6145
Rich Trott <rtrott@gmail.com>
2014-07-10 10:01:23 -0700

    rm obsolete

--- sites/ucsf/test/features/step_definitions/home_screen_steps.rb
+++ sites/ucsf/test/features/step_definitions/home_screen_steps.rb
@@ -6,10 +6,6 @@ Then(/^I should see the home page$/) do
   should have_selector("ol.front")
 end
 
-Given(/^I am on the Native iOS home page$/) do
-  visit "/nativeios.html"
-end
-
 Then(/^I should see a link to "(.*?)"$/) do |href|
   should have_selector(:xpath, "//a[@href='" + href + "']")
 end
