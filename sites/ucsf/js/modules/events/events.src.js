cd89dc23d7fd33b69e532bdafb0c58b5942be94a
Rich Trott <rtrott@gmail.com>
2015-04-27 09:56:00 -0700

    use featured academic events calendar

--- sites/ucsf/js/modules/events/events.src.js
+++ sites/ucsf/js/modules/events/events.src.js
@@ -3,25 +3,8 @@
     angular.module('events', ['rssReader'])
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider
-        .when('/events', {templateUrl: 'events/mainMenu.html'})
-        .when('/events/ucsfevents', {templateUrl: 'events/ucsfEvents.html', controller: 'eventsController'})
-        .when('/events/academicevents', {templateUrl: 'events/academicEvents.html', controller: 'academicEventsController'});
+        .when('/events', {templateUrl: 'events/academicEvents.html', controller: 'academicEventsController'});
     }])
-    .controller(
-        'eventsController',
-        ['$scope', 'rssReaderService', function ($scope, rssReaderService) {
-            var promise = rssReaderService.getRssData(
-                'http://feeds2.feedburner.com/ucsf/event-calendar',
-                $scope,
-                {localStorageKey: 'feed_ucsf_events'}
-            );
-            promise.then(
-                function(feed) {
-                    $scope.feed = feed;
-                }
-            );
-        }]
-    )
     .controller(
         'academicEventsController',
         ['$scope', 'rssReaderService', function ($scope, rssReaderService) {
--- sites/ucsf/test/features/calendars.feature
+++ sites/ucsf/test/features/calendars.feature
@@ -3,16 +3,8 @@ Feature: Calendars
   The users
   Should be able to visit calendars
 
-Scenario: UCSF Events Calendar
+Scenario: Calendar
   Given my localStorage is empty
   And I am on the home page
   And I click "Events"
-  And I click "UCSF Events"
-  Then I should see the "Events" calendar
-
-Scenario: Featured Academic Events Calendar
-  Given my localStorage is empty
-  And I am on the home page
-  And I click "Events"
-  And I click "Featured Academic Events"
   Then I should see the "Featured Academic Events" calendar
